2688a5541fff1518955a8fc1748b38b1b3539ca6
archbaer <pereirakebaili@gmail.com>
2025-11-15 23:34:54 +0000

    minor changes

--- tests/test_load.py
+++ tests/test_load.py
@@ -11,7 +11,7 @@ def processed_dir(tmp_path):
     p = tmp_path / "artifacts" / "processed"
     (p/ "commodities").mkdir(parents=True, exist_ok=True)
 
-    df = pd.DataFrame({"instrument_id": "a0e938006b2acb6dbe5262dbe9c498a9", "date": "2025-06-01","price": 2525.95904761905})
+    df = pd.DataFrame([{"instrument_id": "a0e938006b2acb6dbe5262dbe9c498a9", "date": "2025-06-01","price": 2525.95904761905}])
     df.to_csv( p / "commodities" / "timeseries.csv", index=False)
     return p
 
